58c48acb23eb28c7988b63184dc1b4d390990c7c
lukecross05 <lukedukecross@gmail.com>
2024-04-16 12:27:43 +0100

    connection established to mongodb

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,8 +1,30 @@
 const express = require('express')
 const router = express.Router()
+/*
+    when a request is called to '/' this handler is called.
+*/
 router.get('/', (req, res) => {
     res.json({mssg: 'test get all'})
-
 })
+/*
+    when a request is called to '/:id' this handler is called.
+    id is a param of route.
+*/
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'test get single'})
+})
+
+router.post('/', (req, res) => {
+    res.json({mssg: 'test post workout'})
+})
+
+router.delete('/:id', (req, res) => {
+    res.json({mssg: 'test delete workout'})
+})
+
+router.patch('/:id', (req, res) => {
+    res.json({mssg: 'test update workout'})
+})
+
 
 module.exports = router
