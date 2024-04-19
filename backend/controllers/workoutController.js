ab0bcfc5864351656003f7bae4c3fc6b3deffcbe
lukecross05 <lukedukecross@gmail.com>
2024-04-19 12:50:28 +0100

    created schema and modles

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+const Workout = require('../models/Workouts')
+
+const createWorkout = async (req, res) => {
+    const {title, reps, weight} = req.body
+    try{
+        const workout = await Workout.create({title, reps, weight})
+        res.status(200).json(workout)
+    } catch(error){
+        res.status(400).json({error: error.message})
+    }
+    res.json({mssg: 'test post workout'})
+}
+
+const getWorkouts = async (req, res) => {
+    const workouts = await Workout.find({}).sort({createdAt: -1})
+    res.status(200).json(workouts)
+}
+
+const getWorkout = async (req, res) => {
+    const { id } = req.params
+    if (!mongoose.Types.ObjectId.isvalid(id)){
+        return res.status(404).json({error: 'no such workout'})
+    }
+    const workout = await Workout.findById(id)
+    if(!workout){
+        return res.status(404).json({error: 'No such workout'})
+      }  
+    res.status(200).json(workout)
+}
+
+
+module.exports = {
+    createWorkout,
+    getWorkouts,
+    getWorkout
+}
+  
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,22 +1,24 @@
 const express = require('express')
+const { 
+    createWorkout,
+    getWorkouts,
+    getWorkout
+} = require('../controllers/workoutController')
 const router = express.Router()
 /*
     when a request is called to '/' this handler is called.
 */
-router.get('/', (req, res) => {
-    res.json({mssg: 'test get all'})
-})
+router.get('/', async (req, res) => getWorkouts)
 /*
     when a request is called to '/:id' this handler is called.
     id is a param of route.
 */
-router.get('/:id', (req, res) => {
-    res.json({mssg: 'test get single'})
-})
-
-router.post('/', (req, res) => {
-    res.json({mssg: 'test post workout'})
-})
+router.get('/:id', async (req, res) => getWorkout)
+/*
+    when a post request is sent to default url, an async function is called.
+    it tries to make a variable named workout from the request, if not returns error.
+*/
+router.post('/', async (req, res) => createWorkout)
 
 router.delete('/:id', (req, res) => {
     res.json({mssg: 'test delete workout'})
